b2a16aa7442cd4f117903d674fa186c3b1fe9315
Kayyoom Ali <techali1718@gmail.com>
2025-07-20 21:36:25 +0530

    feat: add Footer component and update Body component to include Footer

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -2,8 +2,17 @@ import React from "react";
 
 const LoginPage = () => {
   return (
-    <div>
-      <p>Hello from login</p>
+    <div className="card bg-primary text-primary-content w-96 flex justify-center">
+      <div className="card-body">
+        <h2 className="card-title">Card title!</h2>
+        <p>
+          A card component has a figure, a body part, and inside body there are title and actions
+          parts
+        </p>
+        <div className="card-actions justify-end">
+          <button className="btn">Buy Now</button>
+        </div>
+      </div>
     </div>
   );
 };
